19e6289aa1c4b9922758f76f1ae857bee46f329f
Ethan Delgado <efd7152@nyu.edu>
2023-11-04 20:45:48 -0400

    created wordle functionality and ability to download package with pip

---
+++ setup.py
@@ -0,0 +1,29 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='3-python-package-exercise-17',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    description='A simple Wordle-like game to play in the terminal.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    package_data={
+        '': ['*.txt'],  #TODO: Add the word list file to the package data
+    },
+    entry_points={
+        'console_scripts': [
+            'wordle-game=python_package_exercise_17.simple_wordle:start_game',
+        ],
+    },
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
