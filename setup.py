e8c24f1ae18fc7b6c8bc8cd99779002c71c2b4be
lc <1877608896@qq.com>
2023-11-08 00:33:19 -0500

    setup part of readme

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 setup(
     name='3-python-package-exercise-17',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Chang Liu',
+    author_email='cl5706@nyu.edu',
     packages=find_packages(),
     description='A simple Wordle-like game to play in the terminal.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     include_package_data=True,
-    package_data={
+    package_data={  
         '': ['*.txt'],  #TODO: Add the word list file to the package data
     },
     entry_points={
